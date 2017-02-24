21a4faf737d85831ad6499739dde394bb0af98d9
charly54hp <charly56hp@gmail.com>
2017-02-23 17:10:07 -0700

    Agregue un label bien verga

--- ERP/Form1.Designer.cs
+++ ERP/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.lblMARTIN = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -51,11 +52,21 @@
             this.lblMARTIN.TabIndex = 1;
             this.lblMARTIN.Text = "Editado por Martin";
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(78, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Delfin verga";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(525, 219);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.lblMARTIN);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
@@ -69,6 +80,7 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label lblMARTIN;
+        private System.Windows.Forms.Label label2;
     }
 }
 
