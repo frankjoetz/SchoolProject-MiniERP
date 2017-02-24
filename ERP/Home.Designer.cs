9bb3d237e3c403c4293adc06127fb28d4776c998
Alex <alex1071756@gmail.com>
2017-02-23 17:59:04 -0800

    Problema arreglado

--- ERP/Home.Designer.cs
+++ ERP/Home.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ERP
 {
-    partial class Form1
+    partial class Home
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -29,64 +29,37 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.lblMARTIN = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(88, 85);
+            this.label1.Location = new System.Drawing.Point(12, 22);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(340, 33);
+            this.label1.Size = new System.Drawing.Size(525, 33);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Proyecto sincronizado :D";
-            // 
-            // lblMARTIN
-            // 
-            this.lblMARTIN.AutoSize = true;
-            this.lblMARTIN.Location = new System.Drawing.Point(230, 155);
-            this.lblMARTIN.Name = "lblMARTIN";
-            this.lblMARTIN.Size = new System.Drawing.Size(93, 13);
-            this.lblMARTIN.TabIndex = 1;
-            this.lblMARTIN.Text = "Editado por Martin";
+            this.label1.Text = "Este será el form que abrirá los demás.";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(329, 143);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 106);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(186, 13);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Agregue uno profe, ahora soy el mejor";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
+            this.label2.Size = new System.Drawing.Size(635, 33);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Agreguen sus forms en la carpeta de su módulo";
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(112, 153);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "ESA Maadre";
-            this.button1.UseVisualStyleBackColor = true;
-            this.label2.Location = new System.Drawing.Point(78, 54);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(64, 13);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Delfin verga";
-            // 
-            // Form1
+            // Home
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(525, 219);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(675, 203);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.lblMARTIN);
             this.Controls.Add(this.label1);
-            this.Name = "Form1";
+            this.Name = "Home";
             this.Text = "Form1";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -96,9 +69,7 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label lblMARTIN;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button button1;
     }
 }
 
