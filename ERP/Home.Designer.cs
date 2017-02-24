3bca3ba308df09e9678e80d2f8d10466424cf841
joanluarloko1 <angel_loko_jose@hotmail.com>
2017-02-23 20:26:58 -0700

    agregue mi windows form de prueba

--- ERP/Home.Designer.cs
+++ ERP/Home.Designer.cs
@@ -56,7 +56,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(675, 203);
+            this.ClientSize = new System.Drawing.Size(675, 328);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Home";
