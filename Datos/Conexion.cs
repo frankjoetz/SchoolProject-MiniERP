0a06a9a340b579dbbb645c30b392ba31265a7c90
oscarg97 <guachicali_oscarg@hotmail.com>
2017-03-20 12:07:38 -0700

    Puse un poco de codigo en la capa de conexion
    Alex no nos ha mandado la cadena de conexion y pues asi :v ...

--- Datos/Conexion.cs
+++ Datos/Conexion.cs
@@ -3,10 +3,55 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace Datos
 {
     class Conexion
     {
+        public SqlConnection conn;
+        public string cadenadeconexion;
+        public SqlCommand cmd;
+        //
+        public Conexion()
+        {
+            this.cadenadeconexion = "";
+            conn = new SqlConnection(cadenadeconexion);
+        }
+        public Boolean conectar() //abrir conexion
+        {
+            bool flag = false;
+            try
+            {
+                conn.Open();
+                if (conn.State == ConnectionState.Open)
+                {
+                    flag = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                
+            }
+            return flag;
+        }
+        public Boolean Desconectar() //cerrar conexion
+        {
+            bool flag = false;
+            try
+            {
+                conn.Close();
+                if (conn.State == ConnectionState.Closed)
+                {
+                    flag = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                
+            }
+            return flag;
+        }
     }
 }
