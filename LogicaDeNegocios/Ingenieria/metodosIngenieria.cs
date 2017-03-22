660c09331c81a5d21fcefd2f1ae020a30bb1be2b
joanluarloko1 <angel_loko_jose@hotmail.com>
2017-03-21 20:28:46 -0700

    ya doy de alta producto

---
+++ LogicaDeNegocios/Ingenieria/metodosIngenieria.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using Datos;
+
+namespace LogicaDeNegocios
+{
+    public class metodosIngenieria
+    {
+        Conexion conectar = new Conexion();
+        public metodosIngenieria()
+        {
+
+        }
+        public int altaMateriaPrima(int idMateria, string Tipo, string descripcion)
+        {
+            int resultado = 0;
+            IngenieriaCRUD ing = new IngenieriaCRUD();
+            try
+            {
+                ing.IdMateria = idMateria;
+                ing.Tipo = Tipo;
+                ing.Descripcion = descripcion;
+                int result = ing.materiaPrima(ing);
+                if (result == 1)
+                {
+                    resultado = 1;
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return resultado;
+
+        }
+    }
+
+}
