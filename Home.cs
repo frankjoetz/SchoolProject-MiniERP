100c272f1494d990771475597231795e9f9c0a55
oscarg97 <guachicali_oscarg@hotmail.com>
2017-04-02 10:31:12 -0700

    Arreglando 2/3
    Esta es la capa principal, pero no si se arreglara ya que no puedo subir carpetas

---
+++ Home.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ERP
+{
+    public partial class Home : Form
+    {
+        public static Produccion.Produccion frmPr = new Produccion.Produccion();
+        public static Ingenieria.Ingenieria frmIn = new Ingenieria.Ingenieria();
+        public static Planeacion.Planeacion frmPl = new Planeacion.Planeacion();
+        public static Ventas.Ventas frmVe = new Ventas.Ventas();
+        public static Dashboard.Dashboard frmDsh = new Dashboard.Dashboard();
+        public static Compras.Compras frmCo = new Compras.Compras();
+        public static LogIn frmLogin = new LogIn();
+
+        public void esconderForms()
+        {
+            frmIn.Hide();
+            frmPl.Hide();
+            frmPr.Hide();
+            frmVe.Hide();
+            frmDsh.Hide();
+            frmCo.Hide();
+        }
+
+        public Home()
+        {
+            InitializeComponent();
+            this.WindowState = FormWindowState.Maximized;
+
+            frmLogin.MdiParent = this;
+            frmIn.MdiParent = this;
+            frmPl.MdiParent = this;
+            frmPr.MdiParent = this;
+            frmVe.MdiParent = this;
+            frmDsh.MdiParent = this;
+            frmCo.MdiParent = this;
+        }
+
+        private void producciónToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            esconderForms();
+            frmPr.Show();
+        }
+
+        private void Home_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ingenieríaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            esconderForms();
+            frmIn.Show();
+        }
+
+        private void planeaciónToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            esconderForms();
+            frmPl.Show();
+        }
+
+        private void inicioToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            esconderForms();
+        }
+
+        private void ventasToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            esconderForms();
+            frmVe.Show();
+        }
+
+        private void dashboardToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            esconderForms();
+            frmDsh.Show();
+        }
+
+        private void almacenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            esconderForms();
+            frmCo.Show();
+        }
+    }
+}
