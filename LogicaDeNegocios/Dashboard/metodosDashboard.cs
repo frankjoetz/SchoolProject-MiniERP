0a9afaa5c801205b04bf340c23ed3669f64bacdc
MisaelMisael <jose.antonionego93@gmail.com>
2017-04-05 17:08:37 -0600

    Ligeros cambios al dashboard
    pues nada, aqui nomas experimentando con el codigo que todos ustedes han
    hecho, atte: Misael Morales el master de masters :v

---
+++ LogicaDeNegocios/Dashboard/metodosDashboard.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaDeNegocios.Dashboard
+{
+    public class metodosDashboard
+    {
+        Datos.BaseDeDatos bd = new Datos.BaseDeDatos();
+
+        public string imprimirAdvertencias()
+        {
+            string contPed = bd.retornarValor("select count(*) as conteo from Pedido", "conteo");
+            return contPed;
+        }
+
+        public string imprimirProdEnProceso()
+        {
+            string contProd = bd.retornarValor("select count(*) as conteo from Produccion", "conteo");
+            return contProd;
+        }
+    }
+}
