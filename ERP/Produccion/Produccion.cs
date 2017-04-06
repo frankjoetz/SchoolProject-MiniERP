f8522f87dfee2a64498b5bce6ff9aa679f36a5f7
Alex <alex1071756@gmail.com>
2017-04-05 20:40:35 -0700

    Varias modificaciones en producción
    Comenté todo mi código
    Optimización de queries
    Organización general
    Creación de métodos
    etc

--- ERP/Produccion/Produccion.cs
+++ ERP/Produccion/Produccion.cs
@@ -7,90 +7,162 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using LogicaDeNegocios.Produccion; // Cargar métodos de producción
 
 namespace ERP.Produccion
 {
     public partial class Produccion : Form
     {
-        LogicaDeNegocios.Produccion.AgregarProduccion metodosAgregar = new LogicaDeNegocios.Produccion.AgregarProduccion();
-        LogicaDeNegocios.Produccion.AdministrarProduccion metodosAdministrar = new LogicaDeNegocios.Produccion.AdministrarProduccion();
+        Metodos met = new Metodos(); // Instanciar métodos de la Lógica de negocios
         
         public Produccion()
         {
             InitializeComponent();
-            this.WindowState = FormWindowState.Maximized;
-
+            // Métodos a llamar cuando el form se instancia
             cboxBuscar.SelectedIndex = 0;
-
+            this.WindowState = FormWindowState.Maximized;
+            
+            // Métodos para primer tab 
+            ActualizarTabla(dgvPlaneaciones);
+            ControlesActivados(1, false);
+            EvaluarNumeroFilas();
         }
 
-        private void Produccion_Load(object sender, EventArgs e)
-        {
-            // Métodos para el primer tab
-            metodosAgregar.LlenarTablaPlaneaciones(dgvPlaneaciones);
-            metodosAgregar.LlenarDetalles(dgvPlaneaciones, txtIdPlaneacion, txtLinea, dtpFechaInicio, dtpFechaEstimada, lblTiempo);
-            metodosAgregar.mostrarDetalle(lblDetalle, dgvPlaneaciones);
-
-            // Métodos para el segundo tab
-            metodosAdministrar.LlenarProducciones(dgvProducciones);
+        private void tabProduccion_SelectedIndexChanged(object sender, EventArgs e)
+        { // Métodos a llamar cuando se cambia de tab
+            ActualizarTabla(dgvProducciones);
+            ActualizarTabla(dgvPlaneaciones);
+            ControlesActivados(1, false);
+            ControlesActivados(2, false);
         }
 
-        // <<<<<<<<<<<<<<<<<<<<<< Inicio de métodos para agregar producción (tab 1) >>>>>>>>>>>>>>>>>>>>>>>>>>>
+        // ====== Eventos relacionados a tab 1 (Agregar una producción) =====
 
         private void txtBuscar_KeyDown(object sender, KeyEventArgs e)
-        {
-            metodosAgregar.Buscar(cboxBuscar, txtBuscar, dgvPlaneaciones);
+        { // Evento de tecla en TextBox búsqueda
+            met.FiltrarPlaneacion(cboxBuscar, txtBuscar, dgvPlaneaciones);
+            ControlesActivados(1, false);
         }
 
-        private void dgvPlaneaciones_Click(object sender, EventArgs e)
-        {
-            metodosAgregar.LlenarDetalles(dgvPlaneaciones, txtIdPlaneacion, txtLinea, dtpFechaInicio, dtpFechaEstimada, lblTiempo);
-            metodosAgregar.mostrarDetalle(lblDetalle, dgvPlaneaciones);
-
+        private void dgvPlaneaciones_CellClick(object sender, DataGridViewCellEventArgs e)
+        { // Evento de click en celda de tabla Planeaciones
+            met.LlenarDetallesPlaneacion(dgvPlaneaciones, txtIdPlaneacion, txtLinea, dtpFechaInicio, dtpFechaEstimada, lblTiempo);
+            met.MostrarDetallePlaneacion(lblDetalle, dgvPlaneaciones);
+            ControlesActivados(1, true);
         }
 
         private void dtpFechaInicio_ValueChanged(object sender, EventArgs e)
-        {
-            metodosAgregar.MostrarTiempo(dtpFechaInicio, dtpFechaEstimada, lblTiempo, dgvPlaneaciones);
+        { // Evento para fecha de inicio modificada
+            met.MostrarTiempo(dtpFechaInicio, dtpFechaEstimada, lblTiempo, dgvPlaneaciones);
         }
-
         
         private void btnGuardar_Click(object sender, EventArgs e)
-        {
-            metodosAgregar.InsertarProduccion(dgvPlaneaciones, dtpFechaInicio, dtpFechaEstimada, txtObservaciones);
-            metodosAgregar.LlenarTablaPlaneaciones(dgvPlaneaciones);
-            metodosAgregar.LlenarDetalles(dgvPlaneaciones, txtIdPlaneacion, txtLinea, dtpFechaInicio, dtpFechaEstimada, lblTiempo);
-            
+        { // Evento para click en botón de "Comenzar a producir"
+            met.InsertarProduccion(dgvPlaneaciones, dtpFechaInicio, dtpFechaEstimada, txtObservaciones);
+            ActualizarTabla(dgvPlaneaciones);
+            ControlesActivados(1, false);
+            EvaluarNumeroFilas();
         }
 
-        // <<<<<<<<<<<<<<<<<<<<<< Final de métodos para agregar producción (tab 1) >>>>>>>>>>>>>>>>>>>>>>>>>>>
+        //  ===== Métodos para eventos de Administrar producciones (tab2) ==== 
 
-
-        private void tabProduccion_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            metodosAdministrar.LlenarProducciones(dgvProducciones);
-            metodosAgregar.LlenarTablaPlaneaciones(dgvPlaneaciones);
+        private void dgvProducciones_CellClick(object sender, DataGridViewCellEventArgs e)
+        { // Evento de click en celda de tabla producciones
+            met.LlenarDetallesProduccion(lblEtapa, prgrsEtapa, dgvProducciones);
+            ControlesActivados(2, true);
+            if (dgvProducciones.SelectedRows[0].Cells["Etapa"].Value.ToString() == "1")
+                btnRetroceder.Enabled = false;
+        }
+        
+        private void btnCancelar_Click(object sender, EventArgs e)
+        { // Evento para click en botón de Cancelar
+            if(MessageBox.Show("¿Estás seguro de querer cancelar esta producción?\nRegresará a planeación.", "Confirmación", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation) == DialogResult.Yes)
+            {
+                if (met.CancelarProduccion(dgvProducciones))
+                {
+                    MessageBox.Show("La producción se ha regresado a planeación.", "Correcto", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    ActualizarTabla(dgvProducciones);
+                    ControlesActivados(2, false);
+                }
+                else
+                    MessageBox.Show("Ocurrió un error cancelando esta producción, contacte al administrador");
+            }
         }
 
         private void btnAvanzar_Click(object sender, EventArgs e)
-        {
-            metodosAdministrar.AvanzarEtapa(dgvProducciones);
-           
-            metodosAdministrar.LlenarProducciones(dgvProducciones);
-            metodosAdministrar.llenarEtapa(lblEtapa, prgrsEtapa, dgvProducciones);
-            validarBotones();
+        { // Evento para click en botón de Avanzar
+            if (met.AvanzarEtapa(dgvProducciones))
+            {
+                ActualizarTabla(dgvProducciones);
+                met.LlenarDetallesProduccion(lblEtapa, prgrsEtapa, dgvProducciones);
+                ControlesActivados(2, false); // Si se llegó a 8, desactiva los controles
+            }
+            else
+            {
+                met.LlenarDetallesProduccion(lblEtapa, prgrsEtapa, dgvProducciones);
+                ActualizarTabla(dgvProducciones);
+                ControlesActivados(2, true); // Si no se llegó a la 8, activa los controles
+            }
         }
 
         private void btnRetroceder_Click(object sender, EventArgs e)
-        {
-            metodosAdministrar.RetrocederEtapa(dgvProducciones);
-            
-            
-            metodosAdministrar.llenarEtapa(lblEtapa, prgrsEtapa, dgvProducciones);
-            validarBotones();
+        { // Evento para click en botón de Retroceder
+            if (met.RetrocederEtapa(dgvProducciones))
+            {
+                ActualizarTabla(dgvProducciones);
+                ControlesActivados(2, false); // Si se llegó a 8, desactiva los controles
+            }
+            else
+            {
+                ActualizarTabla(dgvProducciones);
+                ControlesActivados(2, true); // Si no se llegó a la 8, activa los controles
+            }
 
         }
 
+        // ===== Métodos privados de el formulario ====
+
+        private void ActualizarTabla(DataGridView tabla)
+        { // Método para refrescar valores de tabla
+            switch (tabla.Name)
+            {
+                case "dgvPlaneaciones":
+                    met.ActualizarPlaneaciones(tabla);
+                    break;
+                case "dgvProducciones":
+                    met.ActualizarProducciones(tabla);
+                    break;
+                default: break;
+            }
+        }
+
+        private void ControlesActivados(int tab, bool estado)
+        { // Método versátil para activar o desactivar controles
+            switch (tab)
+            {
+                case 1:
+                    dtpFechaInicio.Enabled = estado;
+                    txtObservaciones.Enabled = estado;
+                    btnGuardar.Enabled = estado;
+                    break;
+                case 2:
+                    btnCancelar.Enabled = estado;
+                    btnRetroceder.Enabled = estado;
+                    btnAvanzar.Enabled = estado;
+                    btnComenzar.Enabled = estado;
+                    nudSegundos.Enabled = estado;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void EvaluarNumeroFilas()
+        {
+            if (dgvPlaneaciones.RowCount == 0)
+                dgvPlaneaciones.Enabled = false;
+        }
+    /*
         private void validarBotones()
         {
             string etapa = metodosAdministrar.RetornarValor(dgvProducciones, "Etapa");
@@ -109,34 +181,7 @@ namespace ERP.Produccion
                 btnAvanzar.Enabled = true;
                 btnRetroceder.Enabled = true;
             }
-        }
-
-        private void btnCancelar_Click(object sender, EventArgs e)
-        {
-            if (MessageBox.Show("¿Estás seguro de querer cancelar esta producción?\nRegresará a planeación.", "Confirmación", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation) == DialogResult.Yes)
-            {
-                if (metodosAdministrar.CancelarProduccion(dgvProducciones))
-                {
-                    MessageBox.Show("La producción se ha regresado a planeación.", "Correcto", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                    actualizarTablaProduccion();
-                } else
-                {
-                    MessageBox.Show("Ocurrió un error cancelando esta producción, contacte al administrador");
-                }
-
-            }
-        }
-
-        private void actualizarTablaProduccion()
-        {
-            metodosAdministrar.LlenarProducciones(dgvProducciones);
-        }
-
-        private void dgvProducciones_CellClick(object sender, DataGridViewCellEventArgs e)
-        {
-            metodosAdministrar.llenarEtapa(lblEtapa, prgrsEtapa, dgvProducciones);
-            validarBotones();
-        }
+        }*/
         
     }
 }
---
+++ LogicaDeNegocios/Produccion/Metodos.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Datos;
+using System.Windows.Forms;
+
+namespace LogicaDeNegocios.Produccion
+{
+    public class Metodos
+    {
+        BaseDeDatos bd = new BaseDeDatos(); // Instancia de la base de datos
+
+        // ◘◘◘◘◘◘◘ Métodos esclusivos para tab 1 ("Agregar producción") ◘◘◘◘◘◘◘
+
+        public void ActualizarPlaneaciones(DataGridView tablaPlaneacion)
+        { // Método para actualizar planeaciones en tab 1
+            bd.llenarTabla("select * from Vista_PlaneacionesAProducir where Status='Listo para producir'", tablaPlaneacion);
+        }
+
+        public void FiltrarPlaneacion(ComboBox columna, TextBox valor, DataGridView tabla)
+        { // Método para filtrar planeaciones dependiendo del tipo
+            bd.llenarTabla("select * from Vista_PlaneacionesAProducir where " + columna.Text + " like '%" + valor.Text + "%' and Status='Listo para producir'", tabla);
+        }
+
+        public void LlenarDetallesPlaneacion(DataGridView tabla, TextBox idPlaneacion, TextBox linea, DateTimePicker inicio, DateTimePicker estimada, Label tiempo)
+        { // Método para llenar automáticamente los controles
+            idPlaneacion.Text = RetornarValor(tabla, "IdPlaneacion");
+            linea.Text = GetLinea(linea, tabla);
+            MostrarTiempo(inicio, estimada, tiempo, tabla);
+        }
+
+        private string GetLinea(TextBox linea, DataGridView tabla)
+        { // Método para convertir descripción de gama en linea a producir
+            switch (RetornarValor(tabla, "tipo"))
+            {
+                case "Gama baja": return "1";
+                case "Gama media": return "2";
+                case "Gama alta": return "3";
+                default: return "0";
+            }
+        }
+
+        public void MostrarTiempo(DateTimePicker inicio, DateTimePicker estimada, Label tiempo, DataGridView tabla)
+        { // Método para editar label, dtp y calcular tiempo
+            DateTime fechaInicio = inicio.Value;
+            float cantidad = float.Parse(RetornarValor(tabla, "Cantidad"));
+            float resultado = cantidad / 60;
+            estimada.Value = fechaInicio.AddDays(resultado);
+            string mensaje = "Se producen 60 computadoras diarias, sus " + cantidad + " computadoras se producirán en aproximadamente " + resultado + " días. \n";
+            tiempo.Text = mensaje;
+        }
+
+        public void MostrarDetallePlaneacion(Label detalle, DataGridView tabla)
+        { // Método para editar el label y mostrar información de la producción
+            string mensaje = "Se producirán " + RetornarValor(tabla, "Cantidad") + " ordenadores de " + RetornarValor(tabla, "Tipo");
+            detalle.Text = mensaje;
+        }
+
+        public void InsertarProduccion(DataGridView tabla, DateTimePicker inicio, DateTimePicker estimada, RichTextBox observaciones)
+        { // Método para agregar registro de producción
+            string ip = RetornarValor(tabla, "idPlaneacion"); // Id de planeación
+            string fi = inicio.Value.ToString("yyyy-MM-dd"); // Fecha de inicio
+            string li = GetLinea(new TextBox(), tabla); // Linea de producción
+            string ff = estimada.Value.ToString("yyyy-MM-dd"); // Fecha estimada
+            string ob = observaciones.Text; 
+            string query = String.Format("INSERT INTO Produccion VALUES (NULL, '{0}', '{1}', '{2}', '1', '{3}', '{4}')", ip, fi, li, ff, ob);
+            if (bd.insertar(query))
+            {
+                EditarStatusPlaneacion(ip);
+                MessageBox.Show("La planeación se ha puesto en producción correctamente :)\n", "Producción en marcha", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
+            else
+                MessageBox.Show("Ocurrió un error agregando esta producción. :(", "Oops", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+        }
+
+        public void EditarStatusPlaneacion(string idPlaneacion)
+        { // Método para Editar el estado de la planeación
+            string consulta = "update Planeacion set status='En proceso' where idPlaneacion=" + idPlaneacion;
+            bd.actualizar(consulta);
+        }
+
+        // ◘◘◘◘◘ Métodos esclusivos para tab 2 ("Administrar producción") ◘◘◘◘◘
+
+        public void ActualizarProducciones(DataGridView tabla)
+        { // Método para actualizar producciones en tab 2
+            bd.llenarTabla("select * from Vista_ProduccionesOverview where Etapa != 8", tabla);
+        }
+
+        public void LlenarDetallesProduccion(Label etapa, ProgressBar barra, DataGridView tabla)
+        { // Método para llenar detalles y progreso de una producción
+            string et = RetornarValor(tabla, "Etapa");
+            string descripcion = bd.retornarValor("select * from Etapas where Id=" + et, "Detalle");
+            etapa.Text = descripcion;
+            barra.Value = int.Parse(et);
+        }
+
+        public bool CancelarProduccion(DataGridView tabla)
+        { // Método para actualizar planeación y eliminar producción
+            string idPl = RetornarValor(tabla, "idPlaneacion");
+            string idPr = RetornarValor(tabla, "idProduccion");
+
+            if (bd.actualizar("update Planeacion set status='Listo para producir' where idPlaneacion=" + idPl))
+                if (bd.eliminar("delete from Produccion where idProduccion=" + idPr))
+                    return true;
+                else
+                    return false;
+            else
+                return false;
+        }
+
+        public bool AvanzarEtapa(DataGridView tabla)
+        { // Método para avanzar etapa y validar si se llegó a la 8
+            int etapa = int.Parse(RetornarValor(tabla, "Etapa")) + 1;
+            string id = RetornarValor(tabla, "idProduccion");
+            bd.actualizar("update Produccion set Etapa=" + etapa + " where idProduccion=" + id);
+            if (etapa == 7)
+                return true;
+            else
+                return false;
+        }
+
+        public bool RetrocederEtapa(DataGridView tabla)
+        { // Método para retroceder etapa y validar si se llegó a 1
+            int etapa = int.Parse(RetornarValor(tabla, "Etapa")) - 1;
+            string id = RetornarValor(tabla, "idProduccion");
+            bd.actualizar("update Produccion set Etapa=" + etapa + " where idProduccion=" + id);
+            if (etapa == 1)
+                return true;
+            else
+                return false;
+        }
+
+        // ◘◘◘◘◘ Métodos generales y esclusivos ◘◘◘◘◘
+
+        private string RetornarValor(DataGridView tabla, string columna)
+        { // Método para retornar el valor de la columna de una fila seleccionada
+            DataGridViewRow fila = tabla.SelectedRows[0];
+            return fila.Cells[columna].Value.ToString();
+        }
+    }
+}
